5150e8fde57af38b5072372898ec97a3489d96c2
art-tal <talanov-artem@ukr.net>
2021-04-28 16:46:14 +0300

    start

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="container-fluid bg-dark mx-0 px-0 py-2 text-secondary">
+  <div class="before_nav container-fluid bg-dark mx-0 px-0 py-2 text-secondary">
     <div class="container d-flex">
 <!--      <div class="row">-->
         <div class="mr-4">
           <label for="choice" class="mr-2 mb-0">Language:</label>
-          <select id="choice" name="languages" v-model="language">
+          <select id="choice"
+                  name="languages"
+                  v-model="language"
+                  class="bg-dark text-light border-0">
             <option v-for="(lang, key) in languages"
                     :key="key"
                     :value="lang"
@@ -14,20 +17,27 @@
         </div>
         <div class="mr-4">
           <label for="currency" class="mr-2 mb-0">Currency:</label>
-          <select name="currency" id="currency" v-model="currency">
+          <select name="currency"
+                  id="currency"
+                  v-model="currency"
+                  class="bg-dark text-light border-0">
           <option v-for="(cur, key) in currencys"
                   :key="key"
                   :value="cur">{{cur}}</option>
         </select>
         </div>
         <div class="flex-grow-1 text-right">
-          <router-link to="/" class="text-white">Account</router-link>
-          <span class="mx-3">|</span>
-          <router-link to="/" class="text-white">Wishlist</router-link>
-          <span class="mx-3">|</span>
-          <router-link to="/" class="text-white">Log In</router-link>
-          <span class="mx-3">|</span>
-          <router-link to="/" class="text-white">Sign Up</router-link>
+          <i class="bi bi-person-circle text-light" @click="togglePersonMenu"></i>
+          <div class="wrap" :class="{'active-wrap' : showPersonMenu}">
+            <router-link to="/" class="person-link text-white">Account</router-link>
+            <span class="mx-3">|</span>
+            <router-link to="/" class="person-link text-white">Wishlist</router-link>
+            <span class="mx-3">|</span>
+            <router-link to="/" class="person-link text-white">Log In</router-link>
+            <span class="mx-3">|</span>
+            <router-link to="/" class="person-link text-white">Sign Up</router-link>
+          </div>
+
         </div>
 <!--      </div>-->
     </div>
@@ -60,13 +70,71 @@ export default {
             "EUR, €",
             "UAH, ₴"
         ],
-        currency: "USD, $"
+        currency: "USD, $",
+
+        showPersonMenu: true,
       }
+    },
+
+  methods: {
+    togglePersonMenu() {
+      this.showPersonMenu = !this.showPersonMenu;
     }
+  }
 
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 
+  .wrap {
+    display: block;
+  }
+
+  .bi-person-circle {
+    display: none;
+    font-size: 1.8rem;
+  }
+
+  .active-wrap {
+    display: block !important;
+    //right: 0!important;
+    transform: translateX(-150px);
+    transition-property: transform;
+    transition-delay: 500ms;
+    transition-duration: 2s;
+    transition-timing-function: linear;
+  }
+
+
+
+  @media (max-width: 767.9px) {
+    .wrap {
+      display: none;
+      //width: 150px;
+      padding: 15px 20px;
+      position: absolute;
+      top: 37px;
+      right: -150px;
+      z-index: 10;
+      background-color: #6c757d;
+      color: #f8f9fa;
+
+      //transition-property: right;
+      //transition-delay: 100ms;
+      //transition-duration: 1s;
+      //transition-timing-function: linear;
+      .person-link {
+        display: block;
+        margin: 0 10px;
+      }
+      span {
+        display: none;
+      }
+    }
+
+    .bi-person-circle {
+      display: block;
+    }
+  }
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,60 +1,133 @@
 <template>
-  <nav class="navbar navbar-expand-lg text-white navbar-light
-   bg-light">
-      <a class="logo navbar-brand" href="#">
+  <div class="container-fluid bg-light p-2">
+    <nav class="container text-dark bg-light d-flex">
+      <router-link to="/" class="mr-5">
         <img src="../assets/image/logo-70.png" alt="logo">
-      </a>
+      </router-link>
 
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button class="btn btn-outline-secondary menu_open"
+              @click="toggleNav"
+              type="button"
+              >
+        <i class="bi bi-list" v-if="showNav"></i>
+        <i class="bi bi-x" v-else></i>
       </button>
 
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <router-link to="/" class="nav-link">Home<span class="sr-only">(current)</span></router-link>
+      <div class="drop mx-4">
+        <ul class="d-flex align-items-center my-0 h-100 p-0 list">
+          <li class="active">
+            <router-link to="/" class="nav_link">Home</router-link>
           </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <li class="dropdown">
+            <a class="nav_link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
               Catalog
             </a>
             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <router-link class="dropdown-item" to="/">Serial furniture</router-link>
-              <router-link class="dropdown-item" to="/">Custom furniture</router-link>
-<!--              <div class="dropdown-divider"></div>-->
-<!--              <a class="dropdown-item" href="#">Something else here</a>-->
+              <router-link class="nav_link d-block" to="/">Serial furniture</router-link>
+              <router-link class="nav_link d-block" to="/">Custom furniture</router-link>
             </div>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/">About</router-link>
+            <router-link class="nav_link" to="/">About</router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" to="/">Contacts</router-link>
+            <router-link class="nav_link" to="/">Contacts</router-link>
           </li>
         </ul>
-<!--        <form class="form-inline my-2 my-lg-0">-->
-<!--          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">-->
-<!--          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>-->
-<!--        </form>-->
-
-        <form class="input-group form-inline my-2 my-lg-0">
-          <input type="text" class="form-control" placeholder="Search" aria-label="Recipient's username" aria-describedby="button-addon2">
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary" type="button" id="button-addon2"><i class="bi bi-search"></i></button>
-          </div>
-        </form>
       </div>
     </nav>
+<!--    <nav class="container navbar navbar-expand-lg text-white navbar-light ">-->
+<!--      <a class="logo navbar-brand" href="#">-->
+<!--        <img src="../assets/image/logo-70.png" alt="logo">-->
+<!--      </a>-->
+
+<!--      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">-->
+<!--        <span class="navbar-toggler-icon"></span>-->
+<!--      </button>-->
+
+<!--      <div class="collapse navbar-collapse" id="navbarSupportedContent">-->
+<!--        <ul class="navbar-nav mr-auto">-->
+<!--          <li class="nav-item active">-->
+<!--            <router-link to="/" class="nav-link">Home<span class="sr-only">(current)</span></router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item dropdown">-->
+<!--            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+<!--              Catalog-->
+<!--            </a>-->
+<!--            <div class="dropdown-menu" aria-labelledby="navbarDropdown">-->
+<!--              <router-link class="dropdown-item" to="/">Serial furniture</router-link>-->
+<!--              <router-link class="dropdown-item" to="/">Custom furniture</router-link>-->
+<!--              &lt;!&ndash;              <div class="dropdown-divider"></div>&ndash;&gt;-->
+<!--              &lt;!&ndash;              <a class="dropdown-item" href="#">Something else here</a>&ndash;&gt;-->
+<!--            </div>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link class="nav-link" to="/">About</router-link>-->
+<!--          </li>-->
+<!--          <li class="nav-item">-->
+<!--            <router-link class="nav-link" to="/">Contacts</router-link>-->
+<!--          </li>-->
+<!--        </ul>-->
+<!--        &lt;!&ndash;        <form class="form-inline my-2 my-lg-0">&ndash;&gt;-->
+<!--        &lt;!&ndash;          <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">&ndash;&gt;-->
+<!--        &lt;!&ndash;          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>&ndash;&gt;-->
+<!--        &lt;!&ndash;        </form>&ndash;&gt;-->
+
+<!--        <form class="input-group form-inline my-2 my-lg-0 w-50">-->
+<!--          <input id="search_field"-->
+<!--                 name="search_field"-->
+<!--                 type="text"-->
+<!--                 class="form-control"-->
+<!--                 placeholder="Search"-->
+<!--                 aria-label="Search"-->
+<!--                 aria-describedby="button-search">-->
+<!--          <div class="input-group-append">-->
+<!--            <button class="btn btn-outline-dark"-->
+<!--                    type="button"-->
+<!--                    id="button-search">-->
+<!--              <i class="bi bi-search"></i>-->
+<!--            </button>-->
+<!--          </div>-->
+<!--        </form>-->
+<!--      </div>-->
+
+<!--      <div class="basket mx-4">-->
+<!--        <div class="btn btn-info position-relative">-->
+<!--          <i class="bi bi-cart" v-if="!basketLength"></i>-->
+<!--          <i class="bi bi-cart-fill" v-else></i>-->
+<!--          <span class="badge badge-primary position-absolute" v-if="basketLength">{{ basketLength }}</span>-->
+
+<!--        </div>-->
+<!--      </div>-->
+<!--    </nav>-->
+
+
+  </div>
+
 </template>
 
 <script>
 export default {
   name: "Nav",
+
+  data() {
+    return {
+      basketLength: 0,
+      showNav: true,
+    }
+  },
+
+  methods: {
+    toggleNav() {
+      this.showNav = !this.showNav;
+    },
+  }
 }
 </script>
 
 <style scoped lang="scss">
 @import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css");
+
   a.logo {
     margin-right: 30px;
     img {
@@ -62,4 +135,67 @@ export default {
     }
 
   }
+
+  button.menu_open {
+    display: none;
+    align-self: center;
+    width: 40px;
+    height: 40px;
+    i {
+      font-size: 2.0rem;
+
+    }
+  }
+
+ul {
+  list-style: none;
+  .nav_link{
+      margin-right: 15px;
+      color: #3c3c3c !important;
+  }
+}
+
+//.collapse.navbar-collapse {
+//  position: absolute;
+//  top: 110px;
+//}
+
+.input-group {
+
+  width: 300px !important;
+  //height: 30px;
+  .form-control {
+    //height: 100%;
+    font-size: 1.4rem;
+  }
+  .bi {
+    font-size: 1.4rem;
+  }
+}
+
+.basket {
+  flex-grow: 1;
+  text-align: right;
+  .btn {
+    border-radius: 50%;
+    .badge {
+      top: -5px;
+      right: -10px;
+      font-size: 1.2rem;
+    }
+  }
+}
+
+@media (min-width: 768px) and (max-width: 991.9px) {
+  button.menu_open {
+    display: block;
+  }
+
+  .drop {
+    display: none;
+    position: absolute !important;
+    top: 110px !important;
+    left: 0;
+  }
+}
 </style>
