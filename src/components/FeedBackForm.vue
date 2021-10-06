c83110925cc53294968c0f322ad4790e58a85af4
art-tal <talanov-artem@ukr.net>
2021-10-06 16:47:21 +0300

    2021.10.06

---
+++ src/components/FeedBackForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container w-25">
+    <h2 class="text-center mx-auto">Feedback form</h2>
+    <form action="" method="get" class="m-auto">
+      <input id="name_user" name="name_user" type="text" placeholder="Name*">
+      <input id="email_user" name="email_user" type="email" placeholder="E-mail*">
+      <textarea name="msg" id="msg" cols="30" rows="5">Your message</textarea>
+      <button type="submit" id="feedback_submit" class="btn btn-success">Send</button>
+    </form>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "FeedBackForm"
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/styles/index";
+
+.container {
+  margin: 2em auto;
+  width: 100%;
+  border: 1px solid $secondary_color;
+  border-radius: 10px;
+  padding: 2em;
+  background-color: #f8f9fa;
+}
+
+h2 {
+  margin: 0 0 1em;
+}
+
+input, button, textarea {
+  display: block;
+  width: 100%;
+  margin-bottom: 1em;
+  padding: 0.5em;
+  border: 1px solid $secondary_color;
+  border-radius: 5px;
+  font-size: 1.6rem;
+
+  &:focus {
+    background-color: #cceeff;
+    border-color: #cceeff;
+}
+  &:hover {
+    border-color: dodgerblue;
+  }
+}
+
+</style>
--- src/views/Contacts.vue
+++ src/views/Contacts.vue
@@ -1,55 +1,91 @@
 <template>
-  <div class="contacts container">
-    <h1>Contacts</h1>
-    <h3>Phones</h3>
-    <div class="managers d-flex">
-      <h4>Manager of site:</h4>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block px-3">(0512) 55-55-55</a>
-    </div>
-    <div class="managers d-flex">
-      <h4>Shop:</h4>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block px-3">(0512) 55-55-55</a>
-    </div>
-    <div class="managers d-flex">
-      <h4>Designers:</h4>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block px-3">(0512) 55-55-55</a>
-    </div>
-    <div class="managers d-flex">
-      <h4>Factory:</h4>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
-      <a href="" class="d-block px-3">(0512) 55-55-55</a>
+  <div class="contacts container py-5">
+    <h1 class="text-center mb-4">Contacts</h1>
+    <div class="row">
+      <div class="col-md-6 col-12">
+        <h3 class="mt-0">Phones</h3>
+        <div class="managers d-flex">
+          <h4>Manager of site:</h4>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block px-3"><img src="../assets/image/icons/icon-viber.png" alt="viber"> (0512) 55-55-55</a>
+        </div>
+        <div class="managers d-flex">
+          <h4>Shop:</h4>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block px-3"><img src="../assets/image/icons/icon-viber.png" alt="viber"> (0512) 55-55-55</a>
+        </div>
+        <div class="managers d-flex">
+          <h4>Designers:</h4>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block px-3"><img src="../assets/image/icons/icon-viber.png" alt="viber"> (0512) 55-55-55</a>
+        </div>
+        <div class="managers d-flex">
+          <h4>Factory:</h4>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block border-right br-secondary px-3">(0512) 55-55-55</a>
+          <a href="" class="d-block px-3"><img src="../assets/image/icons/icon-viber.png" alt="viber"> (0512) 55-55-55</a>
+        </div>
+
+        <h3>Emails</h3>
+        <a href="">rio@gmail.com</a>
+
+        <h3>Our addresses</h3>
+        <div class="address d-flex">
+          <h4>Shop:</h4>
+          <span>Some where</span>
+        </div>
+
+        <h3>Timetable</h3>
+        <div class="time">
+          <p>ПН-СБ: с 9:00 до 18:00</p>
+          <p>ВС: с 9:00 до 16:00 (без обеда)</p>
+        </div>
+      </div>
+      <div class="col-md-6 col-12 map">
+        <h3 class="mt-0">We are on the map</h3>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d6489.335146203816!2d32.01978120306989!3d46.85492287914578!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sru!2sua!4v1633520012443!5m2!1sru!2sua" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+      </div>
     </div>
 
-    <h3>Emails</h3>
-    <a href="">rio@gmail.com</a>
   </div>
 
-  <h3>Our addresses</h3>
-  <div class="address d-flex">
-    <h4>Shop:</h4>
-    <span>Some where</span>
-  </div>
+  <feed-back-form/>
 
-  <h3>Timetable</h3>
-  <div class="time">
-    <p>ПН-СБ: с 9:00 до 18:00</p>
-    <p>ВС: с 9:00 до 16:00 (без обеда)</p>
-  </div>
+
+
+
 </template>
 
 <script>
+import FeedBackForm from "../components/FeedBackForm";
 export default {
-  name: "Contacnts"
+  name: "Contacnts",
+  components: {
+    FeedBackForm
+  },
 }
+
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+@import "../assets/styles/index";
+
+h3 {
+  text-decoration: underline;
+  margin: 2em 0 1em;
+
+}
+
+a {
+  color: $font_color;
+}
+
+img {
+  width: 1.5em;
+}
+
 
 </style>
